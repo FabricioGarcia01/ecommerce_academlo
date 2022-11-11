a5b018fdb20c252e0468cc6dfeddfa99377dc7dc
FabricioGarcia01 <lasften999@hotmail.com>
2022-11-10 19:29:38 -0500

    first commit

---
+++ stock.js
@@ -0,0 +1,5 @@
+let stockProductos = [
+    {id: 1, nombre: "Hoddies", tipo: "buzo", cantidad: 1,precio: 14,img: './src/img/featured1.png'},
+    {id: 2, nombre: "Shirts", tipo: "buzo", cantidad: 1, precio: 24, img: './src/img/featured2.png'},
+    {id: 3, nombre: "Sweatshirts", tipo: "buzo", cantidad: 1, precio: 24, img: './src/img/featured3.png'},
+]
